eb796f398e66f0ddc51d3cbdfb93155adac295b7
mohinigagare3 <mohinigagare3@gmail.com>
2025-08-03 12:12:31 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,33 @@
+
+# Demonstration of All Arithmetics Operators in Python
+
+a=4
+b=2
+
+
+print("value of a is : ",a)
+print("value of b is : ",b)
+
+c=a+b # addition
+print("Addition is : ",c)
+
+c=a-b # substraction
+print("Substraction is : ",c)
+
+c=a*b # multiplication
+print("Multiplication is :",c)
+
+c=a/b # divison, Divison always Return in floating value
+print("Divison is : ",c)
+
+c=a%b # Remainder
+print("Remainder is : ",c)
+
+c=print(2*2) # Power operator
+print("Power of 2 is : ",c)
+
+c = 2**2 # Exponentiation Operator
+print("Exponentiation of 2 is : ",c)
+
+c = a//b # Floor Divison operator
+print("Floor Divison is : ",c)
