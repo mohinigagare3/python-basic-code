56986e67e7b42153aa0ab1aac5000afe43115706
mohinigagare3 <mohinigagare3@gmail.com>
2025-08-03 12:10:15 +0530

    Add files via upload

---
+++ Default_Arguments.py
@@ -0,0 +1,6 @@
+# Demonstration of Default Arguments in Python...
+
+def welcomeFun(name,address="Science Senior Collage Shahada"):
+    print("Hello I am",name,"Studied At : ",address)
+
+welcomeFun("LALIT")
