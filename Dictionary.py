ba8b87925dd6e587ce005b0f2a8399c2845c8a3e
mohinigagare3 <mohinigagare3@gmail.com>
2025-08-03 12:09:22 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+#Demonstration of Dictionary in Python
+
+MyDictionary = {'ID':101, 'Name':"Lalit", 'Age':20}
+
+print(MyDictionary)
+
+print(type(MyDictionary))
+
+print(len(MyDictionary))
+
+MyDictionary['TotalMarks'] = 60
+
+print(MyDictionary)
+
+
