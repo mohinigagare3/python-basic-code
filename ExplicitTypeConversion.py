02a3af3c2ad95987f84e0fa09315c25deeeea928
mohinigagare3 <mohinigagare3@gmail.com>
2025-08-03 12:07:32 +0530

    Add files via upload

---
+++ ExplicitTypeConversion.py
@@ -0,0 +1,29 @@
+# Demonstration of Explicit Type Conversion in Python
+
+a = '4'
+
+# printing and converting a character to an integer
+
+b = ord(a)
+
+print("After converting character into integer : ")
+print(b)
+
+# printing integer converting to hexadecimal string
+
+
+b = hex(56)
+
+# printing hexadecimal value and converting into a octal
+
+print("After converting integer to hexadecimal")
+
+print(b)
+
+b = oct(56)
+
+# printing a octal number
+
+print("After Converting HexaDecimal to Octal : ")
+
+print(b)
