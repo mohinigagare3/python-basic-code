ac0a2e3617f4ccb005dca5645072998deba3871e
mohinigagare3 <mohinigagare3@gmail.com>
2025-08-03 12:09:05 +0530

    Add files via upload

---
+++ Dictionary1.py
@@ -0,0 +1,27 @@
+dict = d1={
+    'Name':"mohini",
+    'ID':100,
+    'Class':"ty bsc",
+    'Year':2024
+}
+
+d2 = {}
+
+''''print(dict)
+print(d1)'''
+
+
+'''d = d1.get('Name')
+print(d)'''
+
+
+''''d = d1.items()
+print(d)'''
+
+
+'''d = d1.keys
+print(d)'''
+
+'''d = d1.setdefault()
+print(d)
+print(d1)'''
